5f698aed2b13ba7bda1fa3d10741eb69a5fad4e0
Nikolay Gekht <nikolay.gekht@gehtsoftusa.com>
2021-06-05 11:01:35 -0400

    - fix nuget packges build, fix dependencies

--- nuget/prepare.bat
+++ nuget/prepare.bat
@@ -1,3 +1,5 @@
-msbuild ../webviewtest.sln /p:Config=Release
+nuget restore ../webviewtest.sln
+msbuild ../webviewtest.sln /t:Clean /p:Configuration=Release
+msbuild ../webviewtest.sln /p:Configuration=Release
 msbuild nuget.proj -t:Prepare
 msbuild nuget.proj -t:NuSpec,NuPack
